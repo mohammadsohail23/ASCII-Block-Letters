ce93e457dde38ee7b3356e43be05a218fed6e39c
Mohammad Z. Sohail <159181150+mohammadsohail23@users.noreply.github.com>
2025-10-10 02:39:07 -0400

    initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Zayed Sohail
+# Fun fact: I do codeacademy for free time
+
+print("ZZZZZ      SSS ")
+print("    Z     S   S")
+print("   Z      S    ")
+print("  Z        SSS ")
+print(" Z            S")
+print("Z         S   S")
+print("ZZZZZ      SSS ")
